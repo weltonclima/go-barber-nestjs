3d29182290fad6351f8f93004ccafa7813ac6624
Welton Lima <welton.c.lima@gmail.com>
2024-12-18 13:05:01 -0300

    testing vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { HelloModule } from './hello/hello.module';
 
 @Module({
-  imports: [],
+  imports: [HelloModule],
 })
 export class AppModule { }
---
+++ src/hello/hello.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('hello')
+export class HelloController {
+
+  @Get()
+  hello(){
+    return "Hello"
+  }
+}
